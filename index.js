6538b9bb26b6143674b1e62e8eb74fe5fd3499f0
AhmedNagii <36850727+AhmedNagii@users.noreply.github.com>
2022-04-24 20:08:36 +0200

    Update code
    Less code and make numbers shorter

--- index.js
+++ index.js
@@ -1,55 +1,34 @@
-const feetRatio  = 3.28084;
-const kilosRatio  = 2.20462;
 
-
-
-let metersInput = document.getElementById("meter-input");
-let feetsInput  =  document.getElementById("feet-input");
-let feetResult  = document.getElementById("feet-result");
-let metersResult=  document.getElementById("meters-result");
-
-let litersInput = document.getElementById("Liters-input");
-let gallonInput = document.getElementById("gallon-input");
-let gallonsResult =document.getElementById("gallon-result");
-let litersResult=  document.getElementById("Liters-result");
-
-
-let kilosInput = document.getElementById("kilos-input");
-let poundsInput = document.getElementById("pounds-input");
-let poundsResult =document.getElementById("pounds-result");
-let kilosResult=  document.getElementById("kilos-result");
+let lenghtEl = document.getElementById("lenght-el");
+let volumeEl = document.getElementById("volume-el");
+let massEl = document.getElementById("mass-el");
 
 
 function coverterMeters(userInput){
-    feetsInput.textContent = userInput;
-
-    metersInput.textContent = userInput;
-    feetResult.textContent = feetRatio * userInput;
-    metersResult.textContent = userInput / feetRatio;
+  
+    lenghtEl.textContent = `${userInput} meters =
+     ${(3.28084 * userInput).toFixed(2)} feet| ${userInput} 
+     feet = ${(userInput / 3.28084).toFixed(2)} meters`
 }
 
  function converLiters(userInput){
-    litersInput.textContent = userInput;
-    gallonInput.textContent = userInput;
-    gallonsResult.textContent = userInput *  0.264172;
-    litersResult.textContent = 3.785412 * userInput; 
+    volumeEl.textContent = `${userInput} liters =
+    ${(userInput *  0.264172).toFixed(2)} Gallons| ${userInput} 
+    Gallons = ${(3.785412 * userInput).toFixed(2)} liters`
 
  }
  function convertPounds(userInput) {
-     kilosInput.textContent = userInput;
-     poundsInput.textContent = userInput;
-    poundsResult.textContent = 0.453592 * userInput;
-    kilosResult.textContent = userInput * 2.20462;
+    massEl.textContent = `${userInput} kilos =
+    ${(userInput * 2.20462).toFixed(2)} Pounds| ${userInput} 
+    Pounds = ${(0.453592 * userInput).toFixed(2)} kilos`
 }
 
 let userInput = document.getElementById("text-box").value;
 
+
 function convert (){
     let userInput = document.getElementById("text-box").value;
-
-
-
- let showUserInput = document.getElementById("user-input");
+    let showUserInput = document.getElementById("user-input");
      showUserInput.textContent =  userInput ;
      coverterMeters(userInput)
      converLiters(userInput)
